9caf1a7fba7c2759b944addaf8ce323b8ea1bf11
sujin <isuzzi11@gmail.com>
2026-01-10 18:06:01 +0900

    Add square function

--- calculator.py
+++ calculator.py
@@ -1,9 +1,15 @@
 # 기본 계산기 수정
 def add(a, b);
 	return a+b
+
 def subtract (a, b);
 	return a-b
+
 def multiply(a, b):
 	return a+b
+
 def divide_free(a, b);
 	return a/b
+
+def square(a,b);
+	return a*a
